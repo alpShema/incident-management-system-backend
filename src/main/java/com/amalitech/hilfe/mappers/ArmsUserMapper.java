175c8a719e9c9fa9560c9400c139dc0df3598050
Shema alphonse <ashema330@gmail.com>
2026-04-30 17:13:37 +0200

    feat(auth): implement ARMS client with SSO integration and user mapping

--- src/main/java/com/amalitech/hilfe/mappers/ArmsUserMapper.java
+++ src/main/java/com/amalitech/hilfe/mappers/ArmsUserMapper.java
@@ -1,4 +1,24 @@
 package com.amalitech.hilfe.mappers;
 
+import com.amalitech.hilfe.auth.dto.ArmsUserInfo;
+import com.amalitech.hilfe.models.User;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+
+@Component
 public class ArmsUserMapper {
+
+    private ArmsUserMapper() {}
+
+    public static User mapArmsUserToImsUser(ArmsUserInfo armsUser) {
+        return User.builder()
+                .id(armsUser.userId())
+                .fullName(armsUser.firstName() + " " + armsUser.lastName())
+                .email(armsUser.email())
+                .profileImg(armsUser.profileImage())
+                .status(true)
+                .permissions(new ArrayList<>())
+                .build();
+    }
 }
