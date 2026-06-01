69a97368f5e5712496c66e4148e5b84313b53377
Shema alphonse <ashema330@gmail.com>
2026-06-01 12:42:39 +0000

    chore(migration): remove primary agent from AgentGroup and add status reason to Incident

--- src/main/resources/db/migration/V38__drop_primary_agent_from_agent_group.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "AgentGroup" DROP CONSTRAINT IF EXISTS fk_agent_group_primary_agent;
-DROP INDEX IF EXISTS idx_agent_group_primary_agent_id;
-ALTER TABLE "AgentGroup" DROP COLUMN IF EXISTS primary_agent_id;
